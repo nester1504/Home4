554b4e523f1172aa2581b7bf6d54a43876f4773e
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-24 00:04:04 +0600

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = new int [8];
+
+void RandomNumber(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100);
+    }
+}
+
+void WriteArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"[{arr[i]}] " );
+    }
+}
+
+RandomNumber(array);
+WriteArray(array);
